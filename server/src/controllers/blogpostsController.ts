b8375ca9fd5912f93cd273ea26cf0396d888f59a
Selina <selinakim343@outlook.com>
2024-01-16 00:21:07 -0500

    feat: BE add create blogpost endpoint

--- server/src/controllers/blogpostsController.ts
+++ server/src/controllers/blogpostsController.ts
@@ -9,3 +9,20 @@ export const getBlogposts: RequestHandler = async (req, res, next) => {
     next(error);
   }
 };
+
+export const createBlogpost: RequestHandler = async (req, res, next) => {
+  const { title, summary, content, thumbnail } = req.body;
+
+  try {
+    const newBlogpost = await BlogpostModel.create({
+      title: title,
+      summary: summary,
+      content: content,
+      thumbnail: thumbnail,
+    });
+
+    res.status(201).json(newBlogpost);
+  } catch (error) {
+    next(error);
+  }
+};
--- server/src/routes/blogpostsRoutes.ts
+++ server/src/routes/blogpostsRoutes.ts
@@ -5,4 +5,6 @@ const router = express.Router();
 
 router.get("/", BlogpostsController.getBlogposts);
 
+router.post("/", BlogpostsController.createBlogpost);
+
 export default router;
