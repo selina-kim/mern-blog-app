06d6f657a953fe1daeaf52aad3ae1004deaf7b55
Selina <selinakim343@outlook.com>
2024-01-17 02:36:56 -0500

    feat: add Poppins and Inter to font stack

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["Poppins", "Inter"],
+      },
       typography: ({ theme }) => ({
         "my-colors": {
           css: {
