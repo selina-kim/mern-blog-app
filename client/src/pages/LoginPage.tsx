70e02351e1e1d4a01db4a2e1989de7d84f9a9aa2
Selina <selinakim343@outlook.com>
2024-01-19 04:21:51 -0500

    feat: FE add empty input field warning before handling login + signup form submit

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -2,50 +2,73 @@ import { useState } from "react";
 
 export function LoginPage() {
   const [password, setPassword] = useState("");
-  const [email, setEmail] = useState("");
+  const [username, setUsername] = useState("");
   const [showPassword, setShowPassword] = useState(false);
 
-  async function handleLogin(e: React.FormEvent) {
+  const [showUsernameEmptyWarning, setShowUsernameEmptyWarning] =
+    useState(false);
+  const [showPasswordEmptyWarning, setShowPasswordEmptyWarning] =
+    useState(false);
+
+  async function checkLogin(e: React.FormEvent) {
     e.preventDefault();
+    if (username.trim() == "") {
+      setShowUsernameEmptyWarning(true);
+    }
+    if (password.trim() == "") {
+      setShowPasswordEmptyWarning(true);
+    } else handleLogin();
   }
+
+  async function handleLogin() {}
+
+  const requiredWarningStyle = "border-red-500 bg-red-50";
+  const requiredWarning = (
+    <div className="mb-2 mt-1 text-xs text-red-500">This field is required</div>
+  );
 
   return (
     <div className="m-auto min-h-[560px] w-fit p-4">
-      <form className="grid w-72 grid-cols-1 gap-y-5" onSubmit={handleLogin}>
-        <h1 className="my-4 text-center text-2xl font-bold">
-          Login with Email
-        </h1>
+      <form className="grid w-72 grid-cols-1" onSubmit={checkLogin}>
+        <h1 className="my-4 text-center text-2xl font-bold">Login</h1>
 
         <div>
-          <label className="mb-1 block text-sm" htmlFor="login-email">
-            Email
+          <label className="mb-1 block text-sm" htmlFor="login-username">
+            Username
           </label>
-          <div className="w-full rounded-md border border-gray-400 p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
+          <div
+            className={`w-full rounded-md border ${showUsernameEmptyWarning ? requiredWarningStyle : "mb-5 border-gray-400"} p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2`}
+          >
             <input
               type="text"
-              id="login-email"
-              className="w-full text-sm focus:outline-none"
-              value={email}
-              placeholder="Email"
+              id="login-username"
+              className="w-full bg-inherit text-sm focus:outline-none"
+              value={username}
+              placeholder="Username"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
-                setEmail(e.target.value);
+                setShowUsernameEmptyWarning(false);
+                setUsername(e.target.value);
               }}
             />
           </div>
+          {showUsernameEmptyWarning && requiredWarning}
         </div>
 
         <div>
           <label className="mb-1 block text-sm" htmlFor="login-password">
             Password
           </label>
-          <div className="flex w-full flex-row gap-2 rounded-md border border-gray-400 p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
+          <div
+            className={`flex w-full flex-row gap-2 rounded-md border ${showPasswordEmptyWarning ? requiredWarningStyle : "mb-5 border-gray-400"} p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2`}
+          >
             <input
               type={showPassword ? "text" : "password"}
               id="login-password"
-              className="w-full text-sm focus:outline-none"
+              className="w-full bg-inherit text-sm focus:outline-none"
               value={password}
               placeholder="Password"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setShowPasswordEmptyWarning(false);
                 setPassword(e.target.value);
               }}
             />
@@ -57,6 +80,7 @@ export function LoginPage() {
               {showPassword ? "Hide" : "Show"}
             </button>
           </div>
+          {showPasswordEmptyWarning && requiredWarning}
         </div>
 
         <button className="my-4 rounded-md bg-violet-500 p-3 font-medium text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
--- client/src/pages/SignupPage.tsx
+++ client/src/pages/SignupPage.tsx
@@ -8,69 +8,105 @@ export function SignupPage() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-  async function handleSignup(e: React.FormEvent) {
+  const [showEmailEmptyWarning, setShowEmailEmptyWarning] = useState(false);
+  const [showUsernameEmptyWarning, setShowUsernameEmptyWarning] =
+    useState(false);
+  const [showPasswordEmptyWarning, setShowPasswordEmptyWarning] =
+    useState(false);
+  const [showConfirmPasswordEmptyWarning, setShowConfirmPasswordEmptyWarning] =
+    useState(false);
+
+  async function checkSignup(e: React.FormEvent) {
     e.preventDefault();
+    if (email.trim() == "") {
+      setShowEmailEmptyWarning(true);
+    }
+    if (username.trim() == "") {
+      setShowUsernameEmptyWarning(true);
+    }
+    if (password.trim() == "") {
+      setShowPasswordEmptyWarning(true);
+    }
+    if (confirmPassword.trim() == "") {
+      setShowConfirmPasswordEmptyWarning(true);
+    } else handleSignup();
   }
+
+  async function handleSignup() {}
+
+  const requiredWarningStyle = "border-red-500 bg-red-50";
+  const requiredWarning = (
+    <div className="mb-2 mt-1 text-xs text-red-500">This field is required</div>
+  );
 
   return (
     <div className="m-auto min-h-[560px] w-fit p-4">
       <form
         id="login-form"
-        className="mx-auto grid w-72 grid-cols-1 gap-y-5"
-        onSubmit={handleSignup}
+        className="mx-auto grid w-72 grid-cols-1"
+        onSubmit={checkSignup}
       >
-        <h1 className="my-4 text-center text-2xl font-bold">
-          Sign up with Email
-        </h1>
+        <h1 className="my-4 text-center text-2xl font-bold">Sign up</h1>
 
         <div>
           <label className="mb-1 block text-sm" htmlFor="signup-email">
             Email
           </label>
-          <div className="w-full rounded-md border-[1px] border-gray-400 p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
+          <div
+            className={`w-full rounded-md border ${showEmailEmptyWarning ? requiredWarningStyle : "mb-5 border-gray-400"} p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2`}
+          >
             <input
               type="text"
               id="signup-email"
-              className="w-full text-sm focus:outline-none"
+              className="w-full bg-inherit text-sm focus:outline-none"
               value={email}
               placeholder="Email"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setShowEmailEmptyWarning(false);
                 setEmail(e.target.value);
               }}
             />
           </div>
+          {showEmailEmptyWarning && requiredWarning}
         </div>
 
         <div>
           <label className="mb-1 block text-sm" htmlFor="signup-username">
             Username
           </label>
-          <div className="w-full rounded-md border-[1px] border-gray-400 p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
+          <div
+            className={`w-full rounded-md border ${showUsernameEmptyWarning ? requiredWarningStyle : "mb-5 border-gray-400"} p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2`}
+          >
             <input
               type="text"
               id="signup-username"
-              className="w-full text-sm focus:outline-none"
+              className="w-full bg-inherit text-sm focus:outline-none"
               value={username}
               placeholder="Username"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setShowUsernameEmptyWarning(false);
                 setUsername(e.target.value);
               }}
             />
           </div>
+          {showUsernameEmptyWarning && requiredWarning}
         </div>
 
         <div>
           <label className="mb-1 block text-sm" htmlFor="signup-password">
             Password
           </label>
-          <div className="flex w-full flex-row gap-2 rounded-md border-[1px] border-gray-400 p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
+          <div
+            className={`flex w-full flex-row gap-2 rounded-md border ${showPasswordEmptyWarning ? requiredWarningStyle : "mb-5 border-gray-400"} p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2`}
+          >
             <input
               type={showPassword ? "text" : "password"}
               id="signup-password"
-              className="w-full text-sm focus:outline-none"
+              className="w-full bg-inherit text-sm focus:outline-none"
               value={password}
               placeholder="Password"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setShowPasswordEmptyWarning(false);
                 setPassword(e.target.value);
               }}
             />
@@ -82,6 +118,7 @@ export function SignupPage() {
               {showPassword ? "Hide" : "Show"}
             </button>
           </div>
+          {showPasswordEmptyWarning && requiredWarning}
         </div>
 
         <div>
@@ -91,14 +128,17 @@ export function SignupPage() {
           >
             Confirm Password
           </label>
-          <div className="flex w-full flex-row gap-2 rounded-md border-[1px] border-gray-400 p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
+          <div
+            className={`flex w-full flex-row gap-2 rounded-md border ${showConfirmPasswordEmptyWarning ? requiredWarningStyle : "mb-5 border-gray-400"} p-3 focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2`}
+          >
             <input
               type={showConfirmPassword ? "text" : "password"}
               id="signup-confirm-password"
-              className="w-full text-sm focus:outline-none"
+              className="w-full bg-inherit text-sm focus:outline-none"
               value={confirmPassword}
               placeholder="Confirm Password"
               onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+                setShowConfirmPasswordEmptyWarning(false);
                 setConfirmPassword(e.target.value);
               }}
             />
@@ -110,6 +150,7 @@ export function SignupPage() {
               {showConfirmPassword ? "Hide" : "Show"}
             </button>
           </div>
+          {showConfirmPasswordEmptyWarning && requiredWarning}
         </div>
 
         <button className="my-4 rounded-md bg-violet-500 p-3 font-medium text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-violet-500 focus-within:ring-offset-2">
