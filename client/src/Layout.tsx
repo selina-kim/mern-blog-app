0614002448ee67df73bc6c0ce25e1b0d5d4f3117
Selina <selinakim343@outlook.com>
2024-01-22 02:49:06 -0500

    fix: FE adjust mobile layout margin to header height

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -9,7 +9,7 @@ function Layout({ onLogoutSuccessful }: LayoutProps) {
   return (
     <>
       <Header onLogoutSuccessful={onLogoutSuccessful} />
-      <div className="mx-auto mt-[69px] h-[calc(100vh-69px)] min-h-fit max-w-3xl px-4 sm:mt-[97px] sm:h-[calc(100vh-97px)]">
+      <div className="mx-auto mt-[65px] h-[calc(100vh-65px)] min-h-fit max-w-3xl px-4 sm:mt-[97px] sm:h-[calc(100vh-97px)]">
         <Outlet />
       </div>
     </>
